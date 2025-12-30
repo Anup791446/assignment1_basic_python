fe4f4688297cf3a43473254c1f45b59f05147d71
Anup791446 <anuppankaj352@gmail.com>
2025-12-30 19:39:25 +0530

    Assignment1

---
+++ basic_operations.py
@@ -0,0 +1,19 @@
+"""
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen.
+"""
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+sum = num1 + num2
+sub = num1 - num2
+mul= num1 * num2
+div = num1 / num2
+print("Addition: ", sum)
+print("subtraction: ", sub)
+print("multiplication: ", mul)
+print("Division: ", div)
